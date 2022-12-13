ad22b48fac8118013450b11921eb85559dda2a3f
jorlar <jqern.larsen@gmail.com>
2022-12-13 19:43:41 +0100

    :sparkles: reformat register controller

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
-const knex = require('knex')
+const knex = require('knex');
+const register = require('./controllers/register');
 
 const db = knex({
   // Enter your own database information here based on what you created
@@ -44,37 +45,7 @@ app.post('/signin', (req, res) => {
     .catch(err => res.status(400).json('wrong credentials'))
 })
 
-app.post('/register', (req, res) => {
-  const { email, name, password } = req.body;
-  const hash = bcrypt.hashSync(password);
-    db.transaction(trx => {
-      trx.insert({
-        hash: hash,
-        email: email
-      })
-      .into('login')
-      .returning('email')
-      .then(loginEmail => {
-        return trx('users')
-          .returning('*')
-          .insert({
-            // If you are using knex.js version 1.0.0 or higher this now returns an array of objects. Therefore, the code goes from:
-            // loginEmail[0] --> this used to return the email
-            // TO
-            // loginEmail[0].email --> this now returns the email
-            email: loginEmail[0].email,
-            name: name,
-            joined: new Date()
-          })
-          .then(user => {
-            res.json(user[0]);
-          })
-      })
-      .then(trx.commit)
-      .catch(trx.rollback)
-    })
-    .catch(err => res.status(400).json('unable to register'))
-})
+app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
 
 app.get('/profile/:id', (req, res) => {
   const { id } = req.params;
