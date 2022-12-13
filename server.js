4f7ef413a7d4390212ebd0f6f5834042202d1b9a
jorlar <jqern.larsen@gmail.com>
2022-12-13 13:48:24 +0100

    :sparkles: Add Bcrypt for passwords

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
 
 const app = express();
 app.use(bodyParser.json())
@@ -45,6 +46,9 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
     const { email, name, password} = req.body;
+    bcrypt.hash(password, null, null, function(err, hash) {
+        // Store hash in you psw db.
+    })
     database.users.push({
         id: '125',
         name: name,
