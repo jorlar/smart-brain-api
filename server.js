c03e676209a6ffdc9c479d9e0a1256a04ce7f759
jorlar <jqern.larsen@gmail.com>
2022-12-13 13:16:23 +0100

    :sparkles: Create server with dummy-data

--- server.js
+++ server.js
@@ -30,12 +30,30 @@ app.get('/', (req, res) => {
     res.send('this is working')
 })
 
+
+//Sign in to app
+
 app.post('/signin', (req, res) => {
     if (req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
         res.json('success');
     } else {
         res.status(400).json('error loging in')
     }
+})
+
+// Register new user 
+
+app.post('/register', (req, res) => {
+    const { email, name, password} = req.body;
+    database.users.push({
+        id: '125',
+        name: name,
+        email: email,
+        password: password,
+        entries: 0,
+        joined: new Date()
+    })
+    res.json(database.users[database.users.length-1]);
 })
 
 app.listen(3000, () => {
